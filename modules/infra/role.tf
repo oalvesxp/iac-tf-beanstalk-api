71e6a7c05f804a6a07656f3c85c34a6d2eb8d9aa
Osvaldo Alves <oalvesxp@gmail.com>
2023-12-11 12:21:29 -0300

    adding aws IAM role of beanstalk

---
+++ modules/infra/role.tf
@@ -0,0 +1,20 @@
+## AWS IAM Role of Beanstalk
+resource "aws_iam_role" "this" {
+  name = "beanstalk-ec2"
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
