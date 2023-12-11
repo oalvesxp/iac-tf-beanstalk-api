fd036cbe25fb107022b396d44df02f78b7be87ec
Osvaldo Alves <oalvesxp@gmail.com>
2023-12-11 12:27:28 -0300

    adding policy for IAM role of beanstalk

--- modules/infra/role.tf
+++ modules/infra/role.tf
@@ -1,4 +1,4 @@
-## AWS IAM Role of Beanstalk
+## AWS IAM role of Beanstalk
 resource "aws_iam_role" "this" {
   name = "beanstalk-ec2"
 
@@ -14,6 +14,42 @@ resource "aws_iam_role" "this" {
         Principal = {
           Service = "ec2.amazonaws.com"
         }
+      },
+    ]
+  })
+}
+
+## AWS IAM role policy of Beanstalk
+resource "aws_iam_role_policy" "name" {
+  name = "beanstalk-ec2"
+  role = aws_iam_role.beanstalk.id
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "cloudwatch:PutMetricData",
+          "ds:CreateComputer",
+          "ds:DescribeDirectories",
+          "ec2:DescribeInstanceStatus",
+          "logs:*",
+          "ssm:*",
+          "ec2messages:*",
+          "ecr:GetAuthorizationToken",
+          "ecr:BatchCheckLayerAvailability",
+          "ecr:GetDownloadUrlForLayer",
+          "ecr:GetRepositoryPolicy",
+          "ecr:DescribeRepositories",
+          "ecr:ListImages",
+          "ecr:DescribeImages",
+          "ecr:BatchGetImage",
+          "s3:*"
+        ]
+        Effect   = "Allow"
+        Resource = "*"
       },
     ]
   })
